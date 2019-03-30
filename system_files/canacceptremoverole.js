f2206f94f69bec4012057ba3f7985f922c3351ef
jackiedissney <49033756+jackiedissney@users.noreply.github.com>
2019-03-31 00:24:51 +0300

    Delete canacceptremoverole.js

--- system_files/canacceptremoverole.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-exports.get = () => {
-    let canacceptremoverole = [
-        "Следящий за Discord",
-        "Тех.администратор Discord"
-    ];
-    return canacceptremoverole;
-}
