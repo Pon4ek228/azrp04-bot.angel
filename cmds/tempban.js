e0c72db506036e46e523cae9eb138c2689c186c4
jackiedissney <49033756+jackiedissney@users.noreply.github.com>
2019-05-10 03:01:52 +0300

    Update tempban.js

--- cmds/tempban.js
+++ cmds/tempban.js
@@ -5,12 +5,12 @@ module.exports.run = async (bot, message, args) => {
     // !tempban gebruiker reden tijd
 
     // Nakijken als je wel het command mag gebruiken.
-    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Sorry je hebt geen toestemming om dit command te gebruiken.");
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("`Недостаточно прав для использования.`");
 
     // User opvragen en nakijken als je wel een user meegeeft.
     var user = message.guild.member(message.mentions.users.first());
 
-    if (!user) return message.channel.send("Je maakt als volgt gebruik van het command: !tempban gebruiker tijd reden.");
+    if (!user) return message.channel.send("`Используйте: !ban [time] [reason]`");
 
     // Nakijken als je de gebruiker wel kunt bannen.
     if (user.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Deze gebruiker kan je niet temp bannen.");
@@ -30,7 +30,7 @@ module.exports.run = async (bot, message, args) => {
         // Een await gaat wachten tot er een "belofte" komt.
         await message.guild.member(user).ban(reason);
 
-        message.channel.send(`${user} is gebanned voor ${tempBanTime}`);
+        message.channel.send(`\`Пользователь\` ${user} \`был заблокирован на\` ${tempBanTime} \`модератором ${member}\``);
 
         // We gaan een timeout zetten voor terug te unbannen.
         setTimeout(function () {
@@ -38,7 +38,7 @@ module.exports.run = async (bot, message, args) => {
             // Hier moet je het ID meegeven om te kunnen unbannen. (Moet zo volgens discord.js).
             message.guild.unban(user.id);
 
-            message.channel.send(`${user} is niet meer gebanned.`);
+            message.channel.send(`${user} \`был разблокирован по истечению срока.\``);
 
         }, ms(tempBanTime));
 
