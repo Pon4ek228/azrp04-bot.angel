5dccda3078ee0e51b0a8b58ddcc0c5e4d8c6a9a3
jackiedissney <49033756+jackiedissney@users.noreply.github.com>
2019-06-23 13:42:27 +0300

    Update unmute.js

--- cmds/unmute.js
+++ cmds/unmute.js
@@ -6,12 +6,12 @@ module.exports.run = async (bot, message, args, con, cfg) => {
 	let target = message.mentions.members.first() || message.guild.members.get(args[0]);
 	if(!target) return message.reply("`Используйте: !unmute [@упоминание]`");
 
-	let role = message.guild.roles.find(r => r.name == 'muted' );
-	if(!role || !target.roles.has(role.id)) return message.channel.send("Этот пользователь не имеет бана чата.");
+	let role = message.guild.roles.find(r => r.name == 'Muted ☊' );
+	if(!role || !target.roles.has(role.id)) return message.channel.send("`Этот пользователь не имеет бана чата.`");
 
 	try {
 		await target.removeRole(role);
-		message.channel.send(`**Пользователю ${target} был снят бан чата модератором ${message.author}.**`);
+		message.channel.send(`\`Пользователю\` ${target} \`был снят бан чата модератором\` ${message.author}\`.\``);
 	} catch(e) {
 		message.channel.send(`**Ошибка: ${e.message}**`);
 	}
