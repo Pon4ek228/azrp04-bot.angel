27a709f79deff064091ec0e12fcfe3e97a5533bf
jackiedissney <49033756+jackiedissney@users.noreply.github.com>
2019-07-30 22:29:37 +0300

    Update moderation.js

--- cmds/moderation.js
+++ cmds/moderation.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("`Используйте: !moderation [@упоминание]`");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("`Данного пользователя нельзя замутить!`");
-  if(!reason) return message.reply("`, пожалуйста, проверье правильность ввода!`");
+  if(!tomute) return message.reply("`, пожалуйста, проверье правильность ввода!`");
 
   let muterole = message.guild.roles.find(`name`, "Модераторы");
 
