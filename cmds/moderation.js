ababedd0ee20febbaa261aa94d45645e1b496a2b
jackiedissney <49033756+jackiedissney@users.noreply.github.com>
2019-07-30 22:38:16 +0300

    Update moderation.js

--- cmds/moderation.js
+++ cmds/moderation.js
@@ -4,27 +4,26 @@ const prefix = "!";
 module.exports.run = async (bot, message, args) => {
 
 
-  if(!message.member.hasPermission("VIEW_CHANNEL")) return message.reply("`Нет прав для выполнения действия.`");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("`Нет прав для выполнения действия.`");
   if(args[0] == "help"){
     message.reply("Usage: !moderation <user>");
     return;
   }
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("`Используйте: !moderation [@упоминание]`");
-  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("`Данного пользователя нельзя замутить!`");
+  if(tomute.hasPermission("BAN_MEMBERS")) return message.reply("`Данного пользователя нельзя назначить!`");
   if(!tomute) return message.reply("`, пожалуйста, проверье правильность ввода!`");
 
   let muterole = message.guild.roles.find(`name`, "Модераторы");
 
  try{
   await tomute.addRole(muterole);
-  message.channel.send(`Модератор назначен.`);
+  message.channel.send(`\`Модератор назначен.\``);
 } catch(e) {
   message.channel.send(`**Ошибка: ${e.message}**`);
 }
 
   await(tomute.addRole(muterole.id));
-//end of module
 }
 
 module.exports.help = {
