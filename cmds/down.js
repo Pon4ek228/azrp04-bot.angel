5c8714aac9d34ea4b963db82fbb27d2945ff665c
jackiedissney <49033756+jackiedissney@users.noreply.github.com>
2019-07-30 23:01:09 +0300

    Create down.js

---
+++ cmds/down.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args, con, cfg) => {
+
+	let target = message.mentions.members.first() || message.guild.members.get(args[0]);
+	if(!target) return message.reply("`Используйте: !down [@упоминание]`");
+
+	let role = message.guild.roles.find(r => r.name == 'Ст. Модератор' );
+	if(!role || !target.roles.has(role.id)) return message.channel.send("`Этот пользователь не является Ст. Модератором`");
+
+	try {
+		await target.removeRole(role);
+		message.channel.send(`\`Старший Модератор\` ${target} \`понижен до Модератора\``);
+	} catch(e) {
+		message.channel.send(`**Ошибка: ${e.message}**`);
+	}
+}
+
+module.exports.help = {
+	name: "down"
+}
