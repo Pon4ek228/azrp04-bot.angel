19e92c91e48f8da551f238b74a36c3ae4b71d2c3
jackiedissney <49033756+jackiedissney@users.noreply.github.com>
2019-07-31 19:03:41 +0300

    Update moderation.js

--- cmds/moderation.js
+++ cmds/moderation.js
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
  try{
   await tomute.addRole(muterole);
   message.channel.send(`\`Пользователь\` ${tomute} \`назначен модератором.\``);
-   const channel = client.channels.find('name', ✅chat)
+   const channel = message.client.channels.find('name', ✅chat)
 channel.send(qqqqqqqqqqqqqqqqq)
 } catch(e) {
   message.channel.send(`**Жууук, ну тут типа ошибка да: ${e.message}**`);
